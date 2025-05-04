3227bffa01842605c6fbeecdc666fec1c13e68a1
Barry nolan <35882086+barrynln@users.noreply.github.com>
2025-05-04 20:32:25 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  root: '.',            // <-- ADD THIS LINE
-  plugins: [react()],
-})
